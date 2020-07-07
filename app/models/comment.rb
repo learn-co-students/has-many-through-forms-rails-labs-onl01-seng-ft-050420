238620f6977188793b9d4c890430977169163f2f
qu1xs <kevinnealmurphy@gmail.com>
2020-07-07 12:54:01 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,10 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_attributes=(user_attributes)
+    if !user_attributes[:username].blank?
+      user = User.find_or_create_by(username: user_attributes[:username])
+      self.user = user
+    end
+  end
 end
