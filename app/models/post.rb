982687a924f7b7b498b282c23410d3f99092ac08
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-07-12 11:00:37 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,13 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-
+  def categories_attributes=(categories_attributes)
+    categories_attributes.values.each do |category_attributes|
+#binding.pry
+      if category_attributes[:name] != ""
+        category = Category.find_or_create_by(category_attributes)
+        self.post_categories.build(category: category)
+      end
+    end
+  end
 end
