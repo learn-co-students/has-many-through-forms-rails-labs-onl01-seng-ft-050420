7c476bede2625ae2c0fa535c1b9ea292f3498f00
leobr <leobrionesherrera@gmail.com>
2020-07-15 15:50:49 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,9 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  accepts_nested_attributes_for :user
+
+  def user_attributes(attr)
+    self.user = User.create(attr)
+  end
 end
