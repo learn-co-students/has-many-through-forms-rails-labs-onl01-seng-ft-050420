0e6eaa0eb9d31caefe26a1d7c19de89c763422a2
Kamrin <kamrinkennedy@gmail.com>
2020-07-17 13:41:50 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,15 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories, reject_if: proc {|attr| attr['name'].blank?}
 
+  # def categories_attributes(attr)
+  #   cat = Category.find_or_create_by(attr)
+  #   self.categories << cat
+  # end
+
+  def uniq_comms 
+    commenters = self.comments.uniq { |comment| comment.user.username }
+  end
 
 end
